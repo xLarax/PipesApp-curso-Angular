adacba88f48f2006bde3bf1e65530f838337342d
“Lara” <lararodriguezarmada@gmail.com>
2025-05-25 21:16:59 +0200

    Añadidos LowerCasePipe, UpperCasePipe, TitleCasePipe, DecimalPipe, PercentPipe, CurrencyPipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+
+  nameLower = signal('lara');
+  nameUpper = signal('LARA');
+  fullName = signal('laRA ROdriGuEz');
+
+
+}
--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe,CurrencyPipe],
   templateUrl: './numbers-page.component.html',
 })
-export default class NumbersPageComponent { }
+export default class NumbersPageComponent {
+
+  totalSells = signal(49_630_248.4573);
+  percentSells = signal(0.4856);
+}
