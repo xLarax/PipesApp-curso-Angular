9d072be3cabe974d5e9075ed05ed31a3c8fe9cfa
“Lara” <lararodriguezarmada@gmail.com>
2025-05-29 22:34:39 +0200

    Añadido DatePipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -12,6 +12,21 @@ export default class BasicPageComponent {
   nameLower = signal('lara');
   nameUpper = signal('LARA');
   fullName = signal('laRA ROdriGuEz');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup)=>{
+
+    const interval = setInterval(()=>{
+      this.customDate.set(new Date());
+      console.log('Tick');
+    }, 1000);
+
+    onCleanup(()=>{
+      clearInterval(interval);
+    });
+
+  });
 
 
 }
