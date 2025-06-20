6328399d5bb58501346a8c37dee2bcd015bec5b1
“Lara” <lararodriguezarmada@gmail.com>
2025-06-20 21:51:03 +0200

    Añadidos I18n Pipes y Content Projection

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.component.html',
+})
+export class CardComponent {
+
+  title = input.required();
+
+ }
--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,8 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Moisés',
+  gender: 'male',
+  age: 42,
+  adress: 'Galicia, Spain'
+};
+
+const client2 = {
+  name: 'Lara',
+  gender: 'female',
+  age: 33,
+  adress: 'Madrid, Spain'
+};
+
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'Sr.',
+    female: 'Sra.',
+  }
+
+  changeClient(){
+
+    if (this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+
+  }
+
+  //i18n Plural
+  clientsMap = signal({
+    '=0': 'no hay clientes',
+    '=1': 'hay 1 cliente',
+    other: 'hay # clientes',
+
+  });
+
+  clients = signal([
+   'Luisa',
+   'Inma',
+   'Aldán',
+   'Zeus',
+   'Nico',
+   'Paula',
+   'Thais',
+   'Matías',
+   'Aria',
+   'Martín',
+  ]);
+
+  deleteClient(){
+    this.clients.update( prev => prev.slice(1));
+  }
+
+
+ }
